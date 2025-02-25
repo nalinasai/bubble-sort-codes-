8fe44f44f0714170b1db1a1906d81c67ca741f72
nalinasai <nalinanalina995@gmail.com>
2025-02-25 13:04:43 +0530

    modify the c++ file that get the input from the user

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,30 +1,37 @@
 #include <iostream>  // Includes the input-output stream library for printing to the console
+#include <vector>
+#include <sstream>
 using namespace std; // To use standard C++ library functions like cout, endl without needing std:: prefix
 
 int main(){
 
-    cout<< "before sorted"<<endl;
-    //initialize the array
-    int array[5] = {2,4,1,6,3};
+    cout<< "Enter the input: ";
+    vector<int> numbers;
+    string inputs;
 
-    for(int h=0; h<5; h++){
-        cout << array[h]<<" ";
+    getline(cin , inputs);
+
+    stringstream stream(inputs);
+
+    int num;
+    while(stream>>num){
+        numbers.push_back(num);
     }
-    //go to the next line
-    cout << endl;
-
-    for (int i=0; i<4; i++){
-        for (int j=0; j<5-i-1; j++){
-            if(array[j]>array[j+1]){
-                swap(array[j],array[j+1]);
+    
+    
+    int n = numbers.size();
+    for (int i=0; i<n-1; i++){
+        for (int j=0; j<n-i-1; j++){
+            if(numbers[j]>numbers[j+1]){
+                swap(numbers[j],numbers[j+1]);
 
             }
         }
     }
 
-    cout << "after sorted"<<endl;
-    for(int k=0; k<5; k++){
-        cout << array[k]<< " ";
+    cout << "after sorted: ";
+    for(int k=0; k<n; k++){
+        cout << numbers[k]<< " ";
     }
 }
 
