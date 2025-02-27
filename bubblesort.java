c029183e19006ce5eb519939b2aa1abe5e791856
nalinasai <nalinanalina995@gmail.com>
2025-02-27 21:17:48 +0530

    implement get and set in the java code. fix the error

--- bubblesort.java
+++ bubblesort.java
@@ -1,34 +1,39 @@
 import java.lang.System;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 class bubblesort{
     public static void main(String args[]){
-        // Initialize an array with unsorted values
-        int[] array = {2,4,1,7,3};
-
-        System.out.println("before sorted: ");
-
-         // Get the length of the array
-        int n = array.length;
         
-        // Print the original unsorted array
-        for (int p=0; p<n; p++){
-            System.out.print(array[p]+" ");
+        System.out.print("Enter the input: ");
+        Scanner scan = new Scanner(System.in);
+        String inputs = scan.nextLine();
+
+        String[] splitstring = inputs.split(" ");
+        int n = splitstring.length;
+
+        ArrayList<Integer> integerList = new ArrayList<>();
+
+        for(int k=0; k<n; k++){
+            int number = Integer.parseInt(splitstring[k]);
+            integerList.add(number);
         }
+        
+        
         for (int i=0; i<n-1; i++){
             // Inner loop for comparing adjacent elements in the array
             for (int j=0; j<n-i-1; j++){
-                if(array[j]>array[j+1]){
-                    int elementfirst = array[j]; // Store the current element in a temporary variable
-                    array[j] = array[j+1];  // Assign the next element to the current position
-                    array[j+1] = elementfirst; // Assign the stored element to the next position
+                if(integerList.get(j)>integerList.get(j+1)){
+                    int elementfirst = integerList.get(j); // Store the current element in a temporary variable
+                    integerList.set(j,integerList.get(j+1));  // Assign the next element to the current position
+                    integerList.set(j+1,elementfirst); // Assign the stored element to the next position
                 }
             }
         }
-        System.out.println();
-        System.out.println("after sorting: ");
+        
+        System.out.print("after sorting: ");
         for(int k=0; k<n; k++){
-            System.out.print(array[k]+" ");
+            System.out.print(integerList.get(k)+" ");
         }
     }
 }
